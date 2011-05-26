277c814f647c0aeb0a6402c2175e696ec7048e0c
Jonathan Smith <jonathan@impacti.org>
2011-05-26 21:43:00 +0100

    Added a nifty "over X stars" review filter. Not connected to anything yet, but it exists...

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,7 @@
 class StatsController < ApplicationController
   def star_reviews(star)
-    @reviews = Review.find_all_by_rating.where("rating >= ?",star)
+    @reviews = Review.where("rating >= ?",star).order('rating desc')
   end
+  
+  
 end
