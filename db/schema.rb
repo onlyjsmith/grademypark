8288afd64eb7c7e9e2a85d62d14c7b8c96546227
Jonathan Smith <jonathan@impacti.org>
2011-06-01 23:06:52 +0100

    Changed column from being called TYPE because rails hates it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110601205955) do
+ActiveRecord::Schema.define(:version => 20110601220352) do
 
   create_table "places", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20110601205955) do
     t.integer  "review_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "content_type"
   end
 
   create_table "users", :force => true do |t|
