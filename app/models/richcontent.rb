a5f0e81d6fae46ea59df79cf676f78f941c4afd0
Jonathan Smith <jonathan@impacti.org>
2011-06-01 23:09:12 +0100

    outline of content types

--- app/models/richcontent.rb
+++ app/models/richcontent.rb
@@ -2,4 +2,10 @@ class Richcontent < ActiveRecord::Base
   belongs_to :review
   
   validates_presence_of :review_id, :on => :create, :message => "Needs to be linked to a review"
+  
+  # Outline for content_type
+  # 1 Picture/image/photo
+  # 2 Blog/user-generated
+  # 3 Commercial
+  
 end
