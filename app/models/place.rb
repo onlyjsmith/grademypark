b4ac4c3ce52484fd1d6af12adee0c863571401a2
Jonathan Smith <jonathan@impacti.org>
2011-06-02 21:56:16 +0100

    added many icons. and changed the place model to have many richcontent

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ActiveRecord::Base
   has_many :reviews
-  has_many :users, :through => :reviews
+  has_many :users, :through => :reviews   
+  has_many :richcontents, :through => :reviews
   
   include HTTParty
   format :json
