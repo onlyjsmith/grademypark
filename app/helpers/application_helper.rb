95dc290638fb68293fd1294017f64d47ea193188
Jonathan Smith <jonathan@impacti.org>
2011-06-03 22:36:50 +0100

    added some stars. slight problem with floating, somewhere...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper    
 
+  def render_stars(rating)
+    content_tag(:div, star_images(rating).html_safe, :class => 'stars')
+  end
+
+  def star_images(rating)
+    (0...rating).map do |position|
+      image_tag "icons/star_fill_sm.png"
+    end.join
+  end 
+
 end
