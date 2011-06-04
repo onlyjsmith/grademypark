1783bb0582f7deadc5c1a0f1b0fc3ff2312957d4
Jonathan Smith <jonathan@impacti.org>
2011-06-04 18:11:09 +0100

    bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["user_info"]["name"]
+      user.username = auth["user_info"]["name"]
     end
   end
   
