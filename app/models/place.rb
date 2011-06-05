429e86cee7017ab0cad1eb8e49aaf78e5aa89233
Jonathan Smith <jonathan@impacti.org>
2011-06-05 13:01:48 +0100

    converted _map to HAML

--- app/models/place.rb
+++ app/models/place.rb
@@ -8,7 +8,7 @@ class Place < ActiveRecord::Base
   
   def self.search(search)
     #go off to ProtectedPlanet API and return results
-    debugger
+    # debugger
     results = Place.get("http://protectedplanet.net/api/search?q=#{search}", :format => :json)
     # results = RestClient.get "http://protectedplanet.net/api/search?q=#{search}", {:accept => :json}
     @ids = []
