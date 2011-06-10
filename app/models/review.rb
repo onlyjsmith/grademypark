96c3389157582a26f2c0f9f63c322337bd760793
Jonathan Smith <jonathan@impacti.org>
2011-06-10 23:26:32 +0100

    made necessary to add place_id to new reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,4 +8,5 @@ class Review < ActiveRecord::Base
   validates_numericality_of :wildness, :only_integer => true, :greater_than => 0, :less_than => 6, :message => 'Between 1 and 5'
   validates_numericality_of :management, :only_integer => true, :greater_than => 0, :less_than => 6, :message => 'Between 1 and 5'
   validates_presence_of :review_text, :on => :create, :message => "can't be blank"
+  validates_presence_of :place_id, :on => :create, :message => "can't be blank - please search from home page"
 end
